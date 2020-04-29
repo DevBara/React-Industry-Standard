e1faba0f2da65f544b497421b2f671d721acef9e
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-29 09:24:12 -0500

    updated Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,16 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
     render() {
         return (
-            <div>
-                <h1> Footer Component</h1>
+            <div className="footerParent">
+                <div className="footer-child">
+                    <div className='footer-titles-parent'>
+                        <h3 className="footer-titles-child">Receive our Quarterly Reports</h3>
+                    </div>
+                    <div className="footer-newsletter-elements">
+                        <input className="footer-input" type="text" placeholder="email"></input>
+                        <button className="footer-button" type="submit">Submit</button>
+                    </div>
+                </div>
             </div>
         )
     }
