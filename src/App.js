d0acc8276ad68d777a26a42c9dceef5a7865519c
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-29 09:15:58 -0500

    routes, links, cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
-
 import './App.css';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
+import Home from './components/Home'
+import KeyPoints from './components/KeyPoints'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-     
-      </header>
+     <Router>
+        <div>
+          <Header />
+          <nav>
+            <ul>
+              <li>
+                <Link className="" to ="/home"> Home</Link>
+              </li>
+              <li>
+                <Link className="" to ="/keypoints"> Key Points</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/Home" component={Home} />
+            <Route path="/KeyPoints" component={KeyPoints} />
+          </Switch>
+        </div>   
+     </Router>
     </div>
   );
 }
