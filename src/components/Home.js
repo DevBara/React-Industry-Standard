a73acf208ec20f88b68a5d325b8ed6b13d2b37fd
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-29 17:01:27 -0500

    added elements

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,14 +5,15 @@ import {NavLink} from 'react-router-dom'
 export default class Home extends Component {
     render() {
         return (
-            <div>
+            <div className="homeContainer">
+                <div className="homeParent">
                 <img className="homeImg" src={AI_HR} alt="hands" />
                 <p className="homeIntro"> As Artificial Intelligence(A.I) becomes more apart of our
                      everyday life, new rules and regulations need to be discussed". 
                      This report highlights very important Key Topics, on ensuring 
                      human rights are being held to high standards and values; as we continue
                      to evolve with A.I. </p>
-                <div>
+                <div className="homeList">
                     <h3>What to expect:</h3>
                     <ul>
                         <li>test</li>
@@ -21,6 +22,7 @@ export default class Home extends Component {
                         <li>test</li>
                     </ul>
                 </div>
+              </div>
             </div>
         )
     }
