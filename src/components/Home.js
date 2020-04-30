ed82d0586603b275b7f2b9a290bf68a27a4308b8
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-30 15:59:14 -0500

    elements

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import AI_HR from '../images/AI_HR.jpg'
-import {NavLink} from 'react-router-dom'
+import DataCover from '../images/DataCover.png'
 
 export default class Home extends Component {
     render() {
         return (
             <div className="homeContainer">
-                <div className="homeParent">
+                <img className="background" src={DataCover} alt="background"/>
                 <img className="homeImg" src={AI_HR} alt="hands" />
                 <p className="homeIntro"> As Artificial Intelligence(A.I) becomes more apart of our
                      everyday life, new rules and regulations need to be discussed". 
@@ -16,14 +16,13 @@ export default class Home extends Component {
                 <div className="homeList">
                     <h3>What to expect:</h3>
                     <ul>
-                        <li>test</li>
-                        <li>test</li>
-                        <li>test</li>
-                        <li>test</li>
+                        <li>Briding AI and Human Rights</li>
+                        <li>Privacy</li>
+                        <li>Disability Rights & Accessibility</li>
                     </ul>
                 </div>
-              </div>
             </div>
+            
         )
     }
 }
