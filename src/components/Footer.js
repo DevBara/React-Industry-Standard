c2cbf90358dfead094ed44eb8ca196b8a4357c27
DevBara <61251493+DevBara@users.noreply.github.com>
2020-04-30 14:48:16 -0500

    test styling & text edits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,7 @@ export default class Footer extends Component {
                 <div className="footer-child">
                     <div className='footer-titles-parent'>
                         <h3>Receive our Quarterly Reports</h3>
+                        <p>Sign up below</p>
                     </div>
                     <div className="footer-newsletter-elements">
                         <input className="footer-input" type="text" placeholder="email"></input>
