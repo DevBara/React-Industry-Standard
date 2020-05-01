53efc5d865434ebb4e3e44d6422790aeb2131cd4
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-30 23:30:51 -0400

    added main sections at top of fold

--- src/components/KeyTopics.js
+++ src/components/KeyTopics.js
@@ -4,7 +4,12 @@ export default class KeyPoints extends Component {
     render() {
         return (
             <div>
-                <h1>Key Point Component</h1>
+                <h1 style="color: grey;">Key Point Component</h1>
+                <div id="headlineArea"> 
+                     <div id="textHeadlineOne"></div>   <div id="aiImageOne"></div>
+                </div>
+                <div id="mainTextAreaOne"> </div>
+
             </div>
         )
     }
