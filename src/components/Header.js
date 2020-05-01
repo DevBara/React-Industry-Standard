f01aab4e9a48f1258a0bdb0f34908cf5ca55ec0d
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-30 22:59:39 -0400

    added div to test pushing to branch

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,9 @@ export default class Header extends Component {
     render() {
         return (
             <div>
+                <div>
+                    
+                </div>
                 
             </div>
         )
