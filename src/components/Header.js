153c5286d0433a76aa93a2310edd8a19e46209f2
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-30 23:06:33 -0400

    added basics for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
+import '../App.css';
+
 
 export default class Header extends Component {
     render() {
         return (
             <div>
-                <div>
-                    
-                </div>
+                <header id="header">
+            How Ai can change the world forever!
+                </header>
                 
             </div>
         )
